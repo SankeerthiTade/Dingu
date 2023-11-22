617f1f2c1db370d1720c9b320c7caffa888cf2f2
DinguSankeerthi <144417542+SankeerthiTade@users.noreply.github.com>
2023-11-22 17:57:17 +0530

    Student.py

--- Student.py
+++ Student.py
@@ -1,47 +1,71 @@
 import streamlit as st
  
-# Base Class: Student
-class Student:
-    def __init__(self, name, age, grade):
-        self._name = name
-        self._age = age
-        self._grade = grade
- 
-    def display_details(self):
-        st.write(f"Student Name: {self._name}")
-        st.write(f"Student Age: {self._age}")
-        st.write(f"Student Grade: {self._grade}")
- 
- 
-# Derived Class: StudentMarks (Inherits from Student)
-class StudentMarks(Student):
-    def __init__(self, name, age, grade, marks):
-        super().__init__(name, age, grade)
-        self._marks = marks
- 
-    def display_details(self):
-        super().display_details()
-        st.write(f"Student Marks: {self._marks}")
- 
- 
-# Streamlit UI
-def main():
-    st.title("Student Details and Marks")
- 
-    # User input for student details
-    name = st.text_input("Enter Student Name:")
-    age = st.number_input("Enter Student Age:")
-    grade = st.text_input("Enter Student Grade:")
- 
-    # User input for marks
-    marks = st.text_input("Enter Student Marks:")
- 
-    # Create an instance of StudentMarks
-    student = StudentMarks(name, age, grade, marks)
- 
-    # Display student details and marks
-    student.display_details()
- 
- 
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+class AuthSystem:
+    def __init__(self):
+        self.users = []
+
+    def register_user(self, username, password):
+        new_user = User(username, password)
+        self.users.append(new_user)
+        print("User registered successfully!")
+
+    def login_user(self, username, password):
+        for user in self.users:
+            if user.username == username and user.password == password:
+                print("Login successful!")
+                return
+        print("Invalid username or password.")
+
+# Web Application
+class WebApp:
+    def __init__(self):
+        self.auth_system = AuthSystem()
+        self.logged_in_user = None
+
+    def display_menu(self):
+        print("1. Register")
+        print("2. Login")
+        print("3. Logout")
+        print("4. Exit")
+
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = input("Enter your choice: ")
+
+            if choice == "1":
+                username = input("Enter username: ")
+                password = input("Enter password: ")
+                self.auth_system.register_user(username, password)
+
+            elif choice == "2":
+                if self.logged_in_user:
+                    print("Already logged in as", self.logged_in_user)
+                else:
+                    username = input("Enter username: ")
+                    password = input("Enter password: ")
+                    self.auth_system.login_user(username, password)
+                    self.logged_in_user = username
+
+            elif choice == "3":
+                if self.logged_in_user:
+                    print("Logout successful!")
+                    self.logged_in_user = None
+                else:
+                    print("Not logged in.")
+
+            elif choice == "4":
+                print("Exiting the application.")
+                break
+
+            else:
+                print("Invalid choice. Please try again.")
+
 if __name__ == "__main__":
-    main()
+    web_app = WebApp()
+    web_app.run()
